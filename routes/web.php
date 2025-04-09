25fe5003011b027cd08b1472d8290b3af809090d
muniz <daniel.muniz997@gmail.com>
2025-04-09 15:38:27 -0300

    routes web

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\ContactController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [PersonController::class, 'index'])->name('people.index');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('people/create', [PersonController::class, 'create'])->name('people.create');
+    Route::get('people/{person}', [PersonController::class, 'show'])->name('people.show');
+    Route::get('people/{person}/edit', [PersonController::class, 'edit'])->name('people.edit');
+    Route::delete('people/{person}', [PersonController::class, 'destroy'])->name('people.destroy');
+    Route::post('people', [PersonController::class, 'store'])->name('people.store');
+    Route::put('people/{person}', [PersonController::class, 'update'])->name('people.update');
+
+    Route::get('people/{person}/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+    Route::get('contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::post('people/{person}/contacts', [ContactController::class, 'store'])->name('contacts.store');
+    Route::put('contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+    Route::delete('contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
 });
