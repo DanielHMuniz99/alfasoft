9c13e62a6a2f08ec2a97152550e5493cd0ab2e1b
muniz <daniel.muniz997@gmail.com>
2025-04-09 14:33:29 -0300

    migrations

---
+++ database/migrations/2025_04_09_114851_create_contracts_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('person_id')->constrained('people')->onDelete('cascade');
+            $table->string('country_code', 5);
+            $table->string('number', 9);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
